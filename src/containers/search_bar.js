c0f512a64ac879260bc38d88faec5f1ba13012b2
Wei Jun <junster90@gmail.com>
2016-06-16 23:47:37 +0800

    added state and callback for search term change

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import connect from 'react-redux';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+
+    this._onInputChange = this._onInputChange.bind(this)
+  }
+
+  _onInputChange(event){
+    this.setState({ term: event.target.value })
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input className="form-control" type="text"/>
+        <input className="form-control" type="text" placeholder="Get a 5 day forecase in your favorite cities" value={this.state.term} onChange={this._onInputChange}/>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
